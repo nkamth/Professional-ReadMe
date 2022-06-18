ccbcfc63a700ce03cd5b310b75037687c526bd9f
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-06-17 23:25:14 -0400

    Created an array of quations to be prompted and created function to initialize app and call the initialize app

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
+// Include packages needed for this application
 const inquirer = require('inquirer')
 
-console.log('ReadMe generator Running');
+//Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your Projects title ?'
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'Description of your Project?',
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'Instructions for installation?',
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'Project Usage information?',
+    },
+    {
+        type:'input',
+        name:'contribution',
+        message:'Information as to how to contribute?',
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'What is your email address?',
+    },
+    {
+        type:'input',
+        name:'github',
+        message:'What is your gitHub username',
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Information to run tests?",
+        default: "npm run test",
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'License ?',
+        choices:['MIT','ISC','GNU GPLv3'],
+        filter(val){
+            return val.toLowerCase();
+        }
+    }
+];
+
+// Create a function to initialize app
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers)=>{
+        console.log(answers);
+        return answers;
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
+
+// Function call to initialize app
+init();
